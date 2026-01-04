4ac2988827ece98e0d482a0babd6fe5fcbd36fd1
tibast <qaultsabitm@gmail.com>
2026-01-04 09:34:03 +0700

    chapter 12.2

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -14,6 +17,20 @@ func (app *application) serve() error {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		app.logger.PrintInfo("caught signal", map[string]string{
+			"signal": s.String(),
+		})
+
+		os.Exit(0)
+	}()
 
 	app.logger.PrintInfo("starting server", map[string]string{
 		"addr": srv.Addr,
